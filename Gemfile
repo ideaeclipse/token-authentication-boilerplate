8d66b80b1a0676060759bccfbe3ed4bed848cc42
Myles Thiessen <thiessem@uwindsor.ca>
2019-04-21 11:28:14 -0700

    switched to mysql, updated user logic and model requirements

--- Gemfile
+++ Gemfile
@@ -5,8 +5,6 @@ ruby '2.6.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
-# Use sqlite3 as the database for Active Record
-gem 'sqlite3', git: "https://github.com/larskanis/sqlite3-ruby", branch: "add-gemspec"
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
@@ -36,6 +34,8 @@ end
 group :development do
 end
 
+gem "yaml_db"
+gem "mysql2", "~> 0.5.2"
 gem 'jwt'
 gem 'rack-cors'
 
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -11,7 +11,7 @@ class UserController < ApplicationController
   def login
     user = User.find_by_username(params[:username])
     if user and user.password == Digest::SHA256.hexdigest(params[:password])
-      user.token = JsonWebToken.encode(user_id: user.id, username: params[:username], user_password: params[:password], nonce: Random.rand(0..1000))
+      user.token = JsonWebToken.encode(time: Time.now.strftime("%d/%m/%Y %H:%M"), user_id: user.id, username: params[:username], user_password: params[:password], nonce: Random.rand(0..1000))
       user.save
       render json: {token: user.token}
     else
@@ -25,7 +25,7 @@ class UserController < ApplicationController
   def index
     value = []
     User.all.each do |user|
-      value << {id: user.id, username: user.username, admin: user.is_admin}
+      value << {id: user.id, username: user.username, is_admin: user.is_admin}
     end
     render json: value
   end
@@ -36,8 +36,7 @@ class UserController < ApplicationController
   def create
     temp = User.find_by_username(params[:username])
     if temp.nil?
-      user = User.new(username: params[:username], password: Digest::SHA256.hexdigest(params[:password]), is_admin: false)
-      user.save
+      User.create!({username: params[:username], password: Digest::SHA256.hexdigest(params[:password]), is_admin: false})
       render json: {status: "User Created"}
     else
       render json: {status: "That username is already taken please try another one"}, status: 400
